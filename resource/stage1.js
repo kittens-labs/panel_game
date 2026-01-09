d5ad054e74787cf544d7d5c0ccc6d6c44a599875
wow <blue.moon.blue.2025@gmail.com>
2026-01-10 08:53:11 +0900

    message change

--- resource/stage1.js
+++ resource/stage1.js
@@ -17,7 +17,7 @@ if(window.panelMainLib.isJP()){
     window.panelMainLib.message("パネルをタッチして", 10,60);
     window.panelMainLib.message("連鎖して消すゲーム!", 10,100);
     window.panelMainLib.message("開始したいパネルを", 10,180);
-    window.panelMainLib.message("タッチしスワイプ", 10,220);
+    window.panelMainLib.message("軽くタッチ", 10,220);
 
     window.panelMainLib.message("※マウスはクリック", 10,300);
     window.panelMainLib.message("をすぐ離してOK", 10,340);
@@ -26,8 +26,8 @@ if(window.panelMainLib.isJP()){
 }else{
     window.panelMainLib.message("Touch panels to chain", 10,60);
     window.panelMainLib.message("and clear them!", 10,100);
-    window.panelMainLib.message("Touch the panel", 10,180);
-    window.panelMainLib.message("to start with and swipe.", 10,220);
+    window.panelMainLib.message("Briefly touch the", 10,180);
+    window.panelMainLib.message("panel to delete.", 10,220);
 
     window.panelMainLib.message("*For mouse, click and", 10,300);
     window.panelMainLib.message("release immediately.", 10,340);
